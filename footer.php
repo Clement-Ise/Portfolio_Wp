274e7b135e27e60b03160ea8ffa8a22308bf3b30
ISE <clementise.formation@gmail.com>
2020-12-16 00:42:37 +0100

    ajout du fichier footer complet

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer class="grid __marger">
+      <div class="footer__1">
+        <a
+          href="mailto:clement.iselin@gmail.com"
+          class="footer_col_work footerstring footerstring_2"
+          >CONTACT</a
+        >
+      </div>
+      <div class="footer__2">
+        <a
+          href="https://github.com/Clement-Ise"
+          class="footer_col_work footerstring footerstring_2"
+          >GITHUB</a
+        >
+      </div>
+      <div class="footer__3">
+        <a
+          href="https://www.linkedin.com/in/clément-iselin/"
+          class="footer_col_work footerstring footerstring_2"
+          >LINKEDIN</a
+        >
+      </div>
+      <p class="footer_cop_work">© Clément ISELIN <span id="year"> </span></p>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
