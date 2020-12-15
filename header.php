cfbce97754842b7bf3ab3d42ccf3acbecc75765f
ISE <clementise.formation@gmail.com>
2020-12-16 00:39:57 +0100

    ajout fichier header work complet

---
+++ header.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="description"
+      content="Clément ISELIN, a junior Front-End developer from FRANCE. I currently learn code at SEM NUMERICA in MONTBELIARD."
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Clément ISELIN | Accueil</title>
+    <?php wp_head();?>
+  </head>
+  <body>
+    <header class="grid __marger">
+      <div class="title-h1">
+        <h1>Front-End Creative Developer</h1>
+      </div>
+      <div class="title-FirstName">
+        <h2>Clément</h2>
+      </div>
+      <div class="title-Name">
+        <h2>ISELIN</h2>
+      </div>
+      <div class="line-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="1"
+          viewBox="0 0 100% 1px"
+        >
+          <line
+            class="lines_head"
+            id="Ligne_10"
+            data-name="Ligne 10"
+            x2="100%"
+            transform="translate(0 0.5)"
+            fill="none"
+            stroke="#ffe654"
+            stroke-width="1"
+          />
+        </svg>
+      </div>
+      <div class="line-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="1"
+          viewBox="0 0 100% 1px"
+        >
+          <line
+            class="lines_head"
+            id="Ligne_10"
+            data-name="Ligne 10"
+            x2="100%"
+            transform="translate(0 0.5)"
+            fill="none"
+            stroke="#ffe654"
+            stroke-width="1"
+          />
+        </svg>
+      </div>
+      <div class="line-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="1"
+          viewBox="0 0 100% 1"
+        >
+          <line
+            class="lines_head"
+            id="Ligne_8"
+            data-name="Ligne 8"
+            x2="100%"
+            transform="translate(0 0.5)"
+            fill="none"
+            stroke="#ffe654"
+            stroke-width="1"
+          />
+        </svg>
+      </div>
+      <div class="line-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="1"
+          viewBox="0 0 100% 1"
+        >
+          <line
+            class="lines_head"
+            id="Ligne_7"
+            data-name="Ligne 7"
+            x2="100%"
+            transform="translate(0 0.5)"
+            fill="none"
+            stroke="#ffe654"
+            stroke-width="1"
+          />
+        </svg>
+      </div>
+    </header>
